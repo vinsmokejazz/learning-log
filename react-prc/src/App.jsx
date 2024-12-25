dddf0fcd9b692cbfc09f8084a169add2666b95ee
vinsmokejazz <vinsmokejazz@gmail.com>
2024-12-25 23:29:09 +0530

    children and props

--- react-prc/src/App.jsx
+++ react-prc/src/App.jsx
@@ -1,41 +1,33 @@
 import { useEffect, useState } from "react";
-import { PostComponent } from "./post";
 
 function App() {
-const [currentTab,setCurrentTab] = useState("feed")
-const [tabData,setData]=useState({});
-const [loading, setLoading]=useState(true);
+  return (
+    <div style={{ display: "flex" }}>
+      <Card>
+        <div style={{color:"white"}}>
+          What do you want to post <br/> <br/>
+          <input type={"text"} />
+        </div>
+      </Card>
+      <Card> hi there </Card>
+    </div>
+  );
 
-useEffect(function(){
-  setLoading(true);
-  fetch('https://jsonplaceholder.typicode.com/todos/1'+ currentTab)
-      .then(async res=>{
-        const json=await res.json();
-        setData(json);
-        setLoading(false);
-      });
-},[currentTab])
-
-
-
-  return <div>
-    <button onClick={function(){
-      setCurrentTab("feed")
-    }} style={{color: currentTab=="feed"? "red" : "black"}}>Feed</button>
-    <button onClick={function(){
-      setCurrentTab("Notifications")
-    }} style={{color: currentTab=="Notifications"? "red" : "black"}}>Notifications</button>
-    <button onClick={function(){
-      setCurrentTab("messages")
-    }} style={{color: currentTab=="messages"? "red" : "black"}}>messages</button>
-    <button onClick={function(){
-      setCurrentTab("Jobs")
-    }} style={{color: currentTab=="Jobs"? "red" : "black"}}>Jobs</button>
-    <br/>
-    {loading ? "loading..." : tabData.title}
-  </div>
-
+  function Card({ children }) {
+    return (
+      <div
+        style={{
+          backgroundColor: "black",
+          borderRadius: 10,
+          color: "white",
+          padding: 20,
+          margin: 10,
+        }}
+      >
+        {children}
+      </div>
+    );
+  }
 }
 
-export default App
-
+export default App;
