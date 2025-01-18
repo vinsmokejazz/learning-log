ace57f9c6cafd46627ff76054e64f2387a4f06d9
vinsmokejazz <vinsmokejazz@gmail.com>
2025-01-18 23:41:54 +0530

    small change

--- react-prc/src/App.jsx
+++ react-prc/src/App.jsx
@@ -1,32 +1,30 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useState } from "react";
 
-function Chat() {
-  const [messages, setMessages] = useState(["Hello!", "How are you?","everything fine"]);
-  const chatBoxRef = useRef(null);
+function App(){ 
+  return <div>
+    <LightBulb/>
+  </div>
+  }
 
-  // Function to simulate adding new messages
-  const addMessage = () => {
-    setMessages((prevMessages) => [...prevMessages, "New message!"]);
-  };
-
-  // Scroll to the bottom whenever a new message is added
-  useEffect(() => {
-    chatBoxRef.current.scrollTop = chatBoxRef.current.scrollHeight;
-  }, [messages]);
-
-  return (
-    <div>
-      <div 
-        ref={chatBoxRef} 
-        style={{ height: "200px", overflowY: "scroll", border: "1px solid black" }}
-      >
-        {messages.map((msg, index) => (
-          <div key={index}>{msg}</div>
-        ))}
-      </div>
-      <button onClick={addMessage}>Add Message</button>
+  function LightBulb(){
+    const[bulbOn, setBulbOn]=useState(true);
+    return <div>
+      <BulbState bulbOn={bulbOn} />
+      <ToggleBulbState setBulbOn={setBulbOn}/>
     </div>
-  );
-}
 
-export default Chat;
+  }
+
+  function BulbState({bulbOn}){
+    return <div>
+      {bulbOn ? "Bulb is on" : "NOT on"}
+    </div>
+  }
+
+  function ToggleBulbState({setBulbOn}){
+    return <div>
+      <button onClick={()=>setBulbOn((prev)=>!prev)}>Toggle bulb</button>
+    </div>
+  }
+
+export default App;
