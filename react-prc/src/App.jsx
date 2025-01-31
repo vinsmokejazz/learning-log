6ef612dba8a21986702cd0a674a767ec5e9f2485
vinsmokejazz <vinsmokejazz@gmail.com>
2025-01-31 22:59:27 +0530

    bulb components

--- react-prc/src/App.jsx
+++ react-prc/src/App.jsx
@@ -1,30 +1,30 @@
-import React, { useState } from "react";
+import { useState } from "react"
 
-function App(){ 
+function App(){
   return <div>
     <LightBulb/>
   </div>
-  }
+}
 
-  function LightBulb(){
-    const[bulbOn, setBulbOn]=useState(true);
-    return <div>
-      <BulbState bulbOn={bulbOn} />
-      <ToggleBulbState setBulbOn={setBulbOn}/>
-    </div>
+function LightBulb(){
+  return <div>
+    <BulbState/>
+    <ToggleBulbState/>
+    <button>Toggle Bulb</button>
+  </div>
+}
 
-  }
+function BulbState(){
+  const [bulbOn,setBulbOn]=useState(true);
+  return <div>
+    {bulbOn? "bulb on": "bulb off"}
+  </div>
 
-  function BulbState({bulbOn}){
-    return <div>
-      {bulbOn ? "Bulb is on" : "NOT on"}
-    </div>
-  }
+}
 
-  function ToggleBulbState({setBulbOn}){
-    return <div>
-      <button onClick={()=>setBulbOn((prev)=>!prev)}>Toggle bulb</button>
-    </div>
-  }
+function ToggleBulbState(){
 
-export default App;
+}
+
+
+export default App
