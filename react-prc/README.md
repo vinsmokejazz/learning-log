5eb78a87ef8e450101ba8238e9ed90bd20f5e99c
vinsmokejazz <vinsmokejazz@gmail.com>
2025-02-01 14:19:06 +0530

    rolling up prop

--- react-prc/README.md
+++ react-prc/README.md
@@ -307,4 +307,46 @@ function Chat() {
 }
 
 export default Chat;
+``` 
+# Rolling up state and Prop drilling eg
+```javascript
+import { useState } from "react"
+
+function App(){
+  return <div>
+    <LightBulb/>
+  </div>
+}
+
+function LightBulb(){
+  const [bulbOn,setBulbOn]=useState(false);
+  return <div>
+    <BulbState bulbOn={bulbOn}/>
+    <ToggleBulbState setBulbOn={setBulbOn} 
+    bulbOn={bulbOn} />
+
+  </div>
+}
+
+function BulbState({bulbOn}){
+  return <div>
+    {bulbOn ? "bulb on": "bulb off"}
+  </div>
+
+}
+
+function ToggleBulbState({bulbOn,setBulbOn}){
+  function toggle(){
+    //setBulbOn(currentState=> !currentState);
+    setBulbOn(!bulbOn);
+  }
+  return <div>
+    <button onClick={toggle}>Toggle Bulb</button>
+  </div>
+
+
+}
+
+
+export default App
 ```
--- react-prc/src/App.jsx
+++ react-prc/src/App.jsx
@@ -1,29 +1,38 @@
-import { useState } from "react"
+import { useState, createContext , useContext } from "react"
+//ideally context stored in seprate file
+const bulbContext=createContext(); //context created always outside the component 
 
 function App(){
+  const [bulbOn,setBulbOn]=useState(true);
+
+  return <div>
+    <bulbContext.Provider value={{
+      bulbOn: bulbOn,
+      setBulbOn: setBulbOn,
+    }}>
+      <Light/>   
+    </bulbContext.Provider> 
+  </div>
+}
+
+function Light(){
+  
   return <div>
     <LightBulb/>
+    <LightSwitch/>
   </div>
 }
 
 function LightBulb(){
-  const [bulbOn,setBulbOn]=useState(false);
-  return <div>
-    <BulbState bulbOn={bulbOn}/>
-    <ToggleBulbState setBulbOn={setBulbOn} 
-    bulbOn={bulbOn} />
-
-  </div>
-}
-
-function BulbState({bulbOn}){
+  const {bulbOn}=useContext(bulbContext);
   return <div>
     {bulbOn ? "bulb on": "bulb off"}
   </div>
 
 }
 
-function ToggleBulbState({bulbOn,setBulbOn}){
+function LightSwitch(){
+  const {bulbOn,setBulbOn}=useContext(bulbContext);
   function toggle(){
     //setBulbOn(currentState=> !currentState);
     setBulbOn(!bulbOn);
