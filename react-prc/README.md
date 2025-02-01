e4b56b4f98e96ad1eef83e878e7c037ee1334693
vinsmokejazz <vinsmokejazz@gmail.com>
2025-02-01 15:06:04 +0530

    better structuring

--- react-prc/README.md
+++ react-prc/README.md
@@ -397,4 +397,62 @@ function LightSwitch(){
 }
 export default App 
 ```
+## Removing ugly code and making bulbprovider as own componet
+```javascript
+import { useState, createContext , useContext } from "react"
+//ideally context stored in seprate file
+const bulbContext=createContext(); //context created always outside the component 
+
+function BulbProvider({children}){
+  const [bulbOn,setBulbOn]=useState(true);
+  return <bulbContext.Provider value={{
+    bulbOn: bulbOn,
+    setBulbOn: setBulbOn,
+  }}>
+    {children}
+  </bulbContext.Provider> 
+
+}
+
+function App(){
+
+  return <div>
+    <BulbProvider>
+      <Light/>
+    </BulbProvider>
+    
+  </div>
+}
+
+function Light(){
+  
+  return <div>
+    <LightBulb/>
+    <LightSwitch/>
+  </div>
+}
+
+function LightBulb(){
+  const { bulbOn }=useContext(bulbContext);
+  return <div>
+    {bulbOn ? 
+    (<img src="https://toppng.com/uploads/preview/light-bulb-on-off-png-11553944387r25q4wkfyw.png"></img> ) :
+
+    (<img src="https://toppng.com/uploads/preview/light-bulb-on-off-png-11553940319kdxsp3rf0i.png"></img> )}
+  </div> 
+
+}
+
+function LightSwitch(){
+  const { bulbOn , setBulbOn }=useContext(bulbContext);
+  function toggle(){
+    //setBulbOn(currentState=> !currentState); best use
+    setBulbOn(!bulbOn);
+  }
+  return <div>
+    <button onClick={toggle}>Toggle Bulb</button>
+  </div>
+}
+export default App
+```s
 
--- react-prc/src/App.jsx
+++ react-prc/src/App.jsx
@@ -2,16 +2,25 @@ import { useState, createContext , useContext } from "react"
 //ideally context stored in seprate file
 const bulbContext=createContext(); //context created always outside the component 
 
-function App(){
+
+function BulbProvider({children}){
   const [bulbOn,setBulbOn]=useState(true);
+  return <bulbContext.Provider value={{
+    bulbOn: bulbOn,
+    setBulbOn: setBulbOn,
+  }}>
+    {children}
+  </bulbContext.Provider> 
+
+}
+
+function App(){
 
   return <div>
-    <bulbContext.Provider value={{
-      bulbOn: bulbOn,
-      setBulbOn: setBulbOn,
-    }}>
-      <Light/>   
-    </bulbContext.Provider> 
+    <BulbProvider>
+      <Light/>
+    </BulbProvider>
+    
   </div>
 }
 
@@ -24,26 +33,22 @@ function Light(){
 }
 
 function LightBulb(){
-  const {bulbOn}=useContext(bulbContext);
+  const { bulbOn }=useContext(bulbContext);
   return <div>
     {bulbOn ? <img src="https://toppng.com/uploads/preview/light-bulb-on-off-png-11553944387r25q4wkfyw.png"></img> :
     <img src="https://toppng.com/uploads/preview/light-bulb-on-off-png-11553940319kdxsp3rf0i.png"></img> }
-  </div>
+  </div> 
 
 }
 
 function LightSwitch(){
-  const {bulbOn,setBulbOn}=useContext(bulbContext);
+  const { bulbOn , setBulbOn }=useContext(bulbContext);
   function toggle(){
-    //setBulbOn(currentState=> !currentState);
+    //setBulbOn(currentState=> !currentState); best use
     setBulbOn(!bulbOn);
   }
   return <div>
     <button onClick={toggle}>Toggle Bulb</button>
   </div>
-
-
 }
-
-
 export default App
