9b5c49eee960a399f8e42c26004ecb463d102b82
vinsmokejazz <vinsmokejazz@gmail.com>
2025-02-03 13:49:56 +0530

    small change

--- react-prc/src/App.jsx
+++ react-prc/src/App.jsx
@@ -1,26 +1,26 @@
-import { useState , useEffect } from "react";
+import { useState, useEffect } from "react";
 import { useFetch, usePostTitle } from "./hooks/useFetch";
 
-function App(){
-  const postTitle=usePostTitle();
-  const[currentPost, setCurrentPost]=useState(1);
-  const { loading,finalData }=useFetch("https://jsonplaceholder.typicode.com/posts/" + currentPost);
-  
-  if(loading){
+function App() {
+  const postTitle = usePostTitle();
+  const [currentPost, setCurrentPost] = useState(1);
+  const { loading, finalData } = useFetch("https://jsonplaceholder.typicode.com/posts/" + currentPost);
+
+  if (loading) {
     return <div>
       Loading...
     </div>
   }
 
   return (
-  <div>
-    {postTitle}
-    <br/>
-    <button onClick={()=> setCurrentPost(1)}>1</button>
-    <button onClick={()=> setCurrentPost(2)}>2</button>
-    <button onClick={()=> setCurrentPost(3)}>3</button>
-    {JSON.stringify(finalData)}
-  </div>
+    <div>
+      {postTitle}
+      <br />
+      <button onClick={() => setCurrentPost(1)}>1</button>
+      <button onClick={() => setCurrentPost(2)}>2</button>
+      <button onClick={() => setCurrentPost(3)}>3</button>
+      {JSON.stringify(finalData)}
+    </div>
   )
 }
 export default App
