9b6e540548615c2086656d7c29d273a119a43315
vinsmokejazz <vinsmokejazz@gmail.com>
2025-02-03 17:39:58 +0530

    debounce function

---
+++ javascript-prc/index.js
@@ -0,0 +1,12 @@
+let currentClock;
+function searchBackend(){
+  console.log("request sent to backend");
+}
+
+function debounceSearch(){
+  clearTimeout(currentClock);
+  currentClock=setTimeout(searchBackend,30);
+}
+
+debounceSearch();
+debounceSearch();
