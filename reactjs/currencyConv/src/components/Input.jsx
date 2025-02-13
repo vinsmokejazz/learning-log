76730341e0d030e1940ca27dd05f2061f03c04b8
vinsmokejazz <vinsmokejazz@gmail.com>
2025-02-13 23:31:33 +0530

    added index

--- reactjs/currencyConv/src/components/Input.jsx
+++ reactjs/currencyConv/src/components/Input.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useId } from "react";
 
 
 function InputBox({
@@ -15,15 +15,18 @@ function InputBox({
 
   
 }) {
+  const amountInputId = useId();  // new 
 
 
   return (
     <div className={`bg-white p-3 rounded-lg text-sm flex  ${className}`}>
       <div className="w-1/2">
-        <label className="text-black/40 mb-2 inline-block">
+        <label  htmlFor={amountInputId} //bind
+        className="text-black/40 mb-2 inline-block">
         {label}
         </label>
         <input
+        id={amountInputId}
           className="outline-none w-full bg-transparent py-1.5"
           type="number"
           placeholder="Amount"
