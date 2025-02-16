f00dc8f34cd6f3301f38b80c6c42f5a996575a94
vinsmokejazz <vinsmokejazz@gmail.com>
2025-02-16 21:24:46 +0530

    added state

--- tailwind/src/components/Otp.jsx
+++ tailwind/src/components/Otp.jsx
@@ -1,20 +1,34 @@
 import Button from "./Button";
 import { useRef, useState } from "react";
 
-function Otp({number}){   //props number shows how many OTP box to render
+function Otp({ number }) {   //props number shows how many OTP box to render
 
   //states to store values/digits inside OTP
   //array cuz we cannot do a loop for hook
-  const[values,setValues]=useState(Array(number).fill(""));
+  const [values, setValues] = useState(Array(number).fill(""));
 
   // Create a ref to hold references to all input boxes.
   // We'll initialize an array of nulls.
-  const inputRefs=useRef(Array(number).fill(null));
+  const inputRefs = useRef(Array(number).fill(null));
 
-  const isDisabled=values.some((val)=>val==="") //checks if any OTP box still empty
+  //some returns a boolean where its argument is function
+  const isDisabled = values.some((val) => val === "") //checks if any OTP box still empty
+
+  //handle change for each box
+  const handleChange=(index,e)=>{
+    const newValue= e.target.value;
+
+    if(!/^\d?$/.test(newValue))return; //allow only single digit
+
+    const newValues=[...values];
+    newValues[index]=newValue;
+    setValues(newValues);
+
+  }
+
 
 
 }
- 
-  
+
+
 export default Otp;
