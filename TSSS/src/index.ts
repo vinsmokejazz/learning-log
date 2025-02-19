6b592f5a6b9101c6cee893b621f7d2fbe47a6594
vinsmokejazz <vinsmokejazz@gmail.com>
2025-02-19 23:05:22 +0530

    interfaces

--- TSSS/src/index.ts
+++ TSSS/src/index.ts
@@ -1,6 +1,57 @@
+
+//objects in ts
 function delay(fn:()=>void){
   setTimeout(fn,1000);
 }
 delay(function(){
   console.log("hello")
 })
+
+let user:{
+  fdn:string,
+  age:number,
+}={
+  fdn: "kirr",
+  age: 10,
+}
+
+
+//interfaces in ts
+interface UserType{
+  firstName: string,
+  lastName: string,
+  age: number
+}
+
+let user1: UserType={
+  firstName:"jj",
+  age: 20,
+  lastName:"kk"
+}
+
+//in function
+function greet(user:UserType){
+
+}
+
+type User={
+
+}
+type StringorNumber = string | number;
+
+type emplyoee={
+  name:string,
+  date:Date;
+}
+type manager={
+  name:string,
+  dept:string,
+}
+
+type teamLead= emplyoee & manager
+
+const tLead : teamLead={
+  name:"hh",
+  date:new Date(),
+  dept:"ddjd"
+}
