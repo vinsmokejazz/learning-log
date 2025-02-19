29d18e6fcc5866410cb4a899e5cc36c39d7273bf
vinsmokejazz <vinsmokejazz@gmail.com>
2025-02-19 23:06:54 +0530

    simple

--- TSSS/src/index.ts
+++ TSSS/src/index.ts
@@ -1,57 +1,66 @@
 
 //objects in ts
-function delay(fn:()=>void){
-  setTimeout(fn,1000);
+function delay(fn: () => void) {
+  setTimeout(fn, 1000);
 }
-delay(function(){
+delay(function () {
   console.log("hello")
 })
 
-let user:{
-  fdn:string,
-  age:number,
-}={
+let user: {
+  fdn: string,
+  age: number,
+} = {
   fdn: "kirr",
   age: 10,
 }
 
 
 //interfaces in ts
-interface UserType{
+interface UserType {
   firstName: string,
   lastName: string,
   age: number
 }
 
-let user1: UserType={
-  firstName:"jj",
+let user1: UserType = {
+  firstName: "jj",
   age: 20,
-  lastName:"kk"
+  lastName: "kk"
 }
 
 //in function
-function greet(user:UserType){
+function greet(user: UserType) {
 
 }
 
-type User={
+type User = {
 
 }
 type StringorNumber = string | number;
 
-type emplyoee={
-  name:string,
-  date:Date;
+type emplyoee = {
+  name: string,
+  date: Date;
 }
-type manager={
-  name:string,
-  dept:string,
+type manager = {
+  name: string,
+  dept: string,
 }
 
-type teamLead= emplyoee & manager
+type teamLead = emplyoee & manager
 
-const tLead : teamLead={
-  name:"hh",
-  date:new Date(),
-  dept:"ddjd"
+const tLead: teamLead = {
+  name: "hh",
+  date: new Date(),
+  dept: "ddjd"
 }
+
+type StringOrNumber = string | number;
+
+function printId(id: StringOrNumber) {
+  console.log(`ID: ${id}`);
+}
+
+printId(101); // ID: 101
+printId("202"); // ID: 202
