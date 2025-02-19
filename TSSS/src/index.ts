cd6e3401ebbf835d538790d6094b3b43eb0ea29e
vinsmokejazz <vinsmokejazz@gmail.com>
2025-02-19 23:11:12 +0530

    rect

--- TSSS/src/index.ts
+++ TSSS/src/index.ts
@@ -66,3 +66,29 @@ function printId(id: StringOrNumber) {
 printId(101); // ID: 101
 printId("202"); // ID: 202
 
+class Rectangle extends Shape {
+  name = "Rectangle";
+
+  constructor(public width: number, public height: number) {
+    super();
+  }
+
+  // Implement the abstract method
+  calculateArea(): number {
+    return this.width * this.height;
+  }
+}
+
+// Another subclass implementing the abstract class
+class Circle extends Shape {
+  name = "Circle";
+
+  constructor(public radius: number) {
+    super();
+  }
+
+  // Implement the abstract method
+  calculateArea(): number {
+    return Math.PI * this.radius * this.radius;
+  }
+}
